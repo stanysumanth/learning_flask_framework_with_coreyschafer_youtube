b91af749735fe0df28b236435004c615d3afa8c6
= <=>
2024-11-19 10:55:14 +0530

    added templates

--- flask_blog/flask_blog.py
+++ flask_blog/flask_blog.py
@@ -3,14 +3,32 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
+posts = [ 
+    {
+        'author': 'stany',
+        'title': 'blog 1',
+        'content': 'first blog post',
+        'date_posted': 'nov 19 2024' ,  
+    },
+
+    {
+        'author': 'sumanth',
+        'title': 'blog 2',
+        'content': 'second blog post',
+        'date_posted': 'nov 19 2024',   
+    }
+
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='about')
 
 if __name__=="__main__":
     app.run(debug=True)
